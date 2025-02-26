9159eec61861faf7b1868b8546c452fc02910893
Gari885 <garinovoselskyyjaka@gmail.com>
2025-02-26 18:55:46 +0100

    Creado en java las dos clases del ejercicio dos

---
+++ EjerciciosDiagrama/src/org/cuatrovientos/dam/entorno/ejercicio2/Invoice.java
@@ -0,0 +1,20 @@
+package org.cuatrovientos.dam.entorno.ejercicio2;
+
+public class Invoice {
+	private String customer;
+
+	public Invoice(String customer) {
+		super();
+		this.customer = customer;
+	}
+	public void add(Product p) {
+		
+	}
+	public void remove(int i) {
+		
+	}
+	
+	public float total() {
+		
+	}
+}
---
+++ EjerciciosDiagrama/src/org/cuatrovientos/dam/entorno/ejercicio2/Product.java
@@ -0,0 +1,24 @@
+package org.cuatrovientos.dam.entorno.ejercicio2;
+
+public class Product {
+	private String name;
+	private int qty;
+	private float price;
+	public Product() {
+	}
+	public Product(String name, int qty, float price) {
+		super();
+		this.name = name;
+		this.qty = qty;
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", qty=" + qty + ", price=" + price + "]";
+	}
+	
+	public float total() {
+		return price;
+	}
+	
+}
